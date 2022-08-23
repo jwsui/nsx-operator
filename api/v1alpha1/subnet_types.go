b3a59967819c0eb575959828fe16563b66f1b29a
jsui <jsui@vmware.com>
2022-08-23 09:24:24 +0800

    add Subnet CRD
    This is initial commit for adding Subnet CRD from kubebuilder, codes
    will be added and layout will be refactored based on this commit.

---
+++ api/v1alpha1/subnet_types.go
@@ -0,0 +1,51 @@
+/* Copyright © 2021 VMware, Inc. All Rights Reserved.
+   SPDX-License-Identifier: Apache-2.0 */
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// SubnetSpec defines the desired state of Subnet
+type SubnetSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of Subnet. Edit subnet_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// SubnetStatus defines the observed state of Subnet
+type SubnetStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// Subnet is the Schema for the subnets API
+type Subnet struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   SubnetSpec   `json:"spec,omitempty"`
+	Status SubnetStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// SubnetList contains a list of Subnet
+type SubnetList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Subnet `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Subnet{}, &SubnetList{})
+}
---
+++ controllers/subnet_controller.go
@@ -0,0 +1,49 @@
+/* Copyright © 2021 VMware, Inc. All Rights Reserved.
+   SPDX-License-Identifier: Apache-2.0 */
+
+package controllers
+
+import (
+	"context"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	nsxvmwarecomv1alpha1 "github.com/vmware-tanzu/nsx-operator/api/v1alpha1"
+)
+
+// SubnetReconciler reconciles a Subnet object
+type SubnetReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+//+kubebuilder:rbac:groups=nsx.vmware.com,resources=subnets,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=nsx.vmware.com,resources=subnets/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=nsx.vmware.com,resources=subnets/finalizers,verbs=update
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the Subnet object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.10.0/pkg/reconcile
+func (r *SubnetReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	_ = log.FromContext(ctx)
+
+	// TODO(user): your logic here
+
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *SubnetReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&nsxvmwarecomv1alpha1.Subnet{}).
+		Complete(r)
+}
