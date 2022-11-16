adc805033008e4c5ebe8db47cac0a6034f45f289
jsui <jsui@vmware.com>
2022-11-16 15:51:35 +0800

    Code refactor
    Changes include:
    1. Add vpc_handler.
    2. Update namespace_handler.

--- pkg/controllers/subnet/subnet_controller.go
+++ pkg/controllers/subnet/subnet_controller.go
@@ -3,6 +3,8 @@ package subnet
 import (
 	"context"
 	"github.com/vmware-tanzu/nsx-operator/pkg/apis/v1alpha1"
+	"github.com/vmware-tanzu/nsx-operator/pkg/controllers/common"
+	"github.com/vmware-tanzu/nsx-operator/pkg/logger"
 	apimachineryruntime "k8s.io/apimachinery/pkg/runtime"
 	"runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -10,13 +12,30 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/controller"
 )
 
-// SubnetSetReconciler SubnetSetReconciler reconciles a SubnetSet object
+var (
+	log                     = logger.Log
+	ResultNormal            = common.ResultNormal
+	ResultRequeue           = common.ResultRequeue
+	ResultRequeueAfter5mins = common.ResultRequeueAfter5mins
+	MetricResType           = common.MetricResTypeSecurityPolicy
+)
+
+// SubnetReconciler reconciles a SubnetSet object
 type SubnetReconciler struct {
 	Client client.Client
 	Scheme *apimachineryruntime.Scheme
 }
 
 func (r *SubnetReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	obj := &v1alpha1.SecurityPolicy{}
+	log.Info("reconciling securitypolicy CR", "securitypolicy", req.NamespacedName)
+	//TODO add service
+	//metrics.CounterInc(r.Service.NSXConfig, metrics.ControllerSyncTotal, MetricResType)
+
+	if err := r.Client.Get(ctx, req.NamespacedName, obj); err != nil {
+		log.Error(err, "unable to fetch Subnet CR", "req", req.NamespacedName)
+		return ResultNormal, client.IgnoreNotFound(err)
+	}
 	return ctrl.Result{}, nil
 }
 
