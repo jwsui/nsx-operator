e0176a548465bd98ac69bdc6a0c2c17af9dcb6cb
jsui <jsui@vmware.com>
2022-12-09 16:34:39 +0800

    add builder

--- pkg/nsx/services/common/types.go
+++ pkg/nsx/services/common/types.go
@@ -31,6 +31,7 @@ const (
 	TagScopeNCPVNETInterface     string = "ncp/vnet_interface"
 	TagScopeVPCCRName            string = "nsx-op/vpc_cr_name"
 	TagScopeVPCCRUID             string = "nsx-op/vpc_cr_uid"
+	TagScopeSubnetCRName         string = "nsx-op/subnet_cr_name"
 	TagScopeSubnetCRUID          string = "nsx-op/subnet_cr_uid"
 
 	GCInterval    = 60 * time.Second
--- pkg/nsx/services/subnet/builder.go
+++ pkg/nsx/services/subnet/builder.go
@@ -1,11 +1,48 @@
 package subnet
 
 import (
-	"github.com/vmware-tanzu/nsx-operator/pkg/apis/v1alpha1"
+	"fmt"
+
 	"github.com/vmware/vsphere-automation-sdk-go/services/nsxt/model"
+
+	"github.com/vmware-tanzu/nsx-operator/pkg/apis/v1alpha1"
+	"github.com/vmware-tanzu/nsx-operator/pkg/nsx/services/common"
+)
+
+var (
+	String = common.String
+	Int64  = common.Int64
 )
 
 func (service *SubnetService) buildSubnet(obj *v1alpha1.Subnet) (*model.VpcSubnet, error) {
-	nsxSubnet := &model.VpcSubnet{}
+	nsxSubnet := &model.VpcSubnet{
+		Id:          String(fmt.Sprintf("subnet_%s", obj.UID)),
+		DisplayName: String(fmt.Sprintf("%s-%s", obj.ObjectMeta.Namespace, obj.ObjectMeta.Name)),
+		AccessMode:  String(string(obj.Spec.AccessMode)),
+	}
+	nsxSubnet.IpAddresses = append(nsxSubnet.IpAddresses, obj.Spec.IPAddresses...)
+	nsxSubnet.Tags = service.buildBasicTags(obj)
 	return nsxSubnet, nil
+}
+
+func (service *SubnetService) buildBasicTags(obj *v1alpha1.Subnet) []model.Tag {
+	tags := []model.Tag{
+		{
+			Scope: String(common.TagScopeCluster),
+			Tag:   String(getCluster(service)),
+		},
+		{
+			Scope: String(common.TagScopeNamespace),
+			Tag:   String(obj.ObjectMeta.Namespace),
+		},
+		{
+			Scope: String(common.TagScopeSubnetCRName),
+			Tag:   String(obj.ObjectMeta.Name),
+		},
+		{
+			Scope: String(common.TagScopeSubnetCRUID),
+			Tag:   String(string(obj.UID)),
+		},
+	}
+	return tags
 }
