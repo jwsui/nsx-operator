44d2bad2db3a019c32bdf6b0f3d4daad7af2caed
jsui <jsui@vmware.com>
2023-02-24 16:53:59 +0800

    [don't pick] for test

--- pkg/controllers/subnet/subnet_controller.go
+++ pkg/controllers/subnet/subnet_controller.go
@@ -52,10 +52,11 @@ func (r *SubnetReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctr
 		log.Error(err, fmt.Sprintf("failed to get VPC under namespace: %s.\n", obj.Namespace))
 		return ResultRequeue, err
 	}
-	// Only one VPC under each namespace.
-	parameters := r.Service.GetSubnetParamFromPath(vpcList.Items[0].Status.NSXResourcePath)
+	//// Only one VPC under each namespace.
+	//parameters := r.Service.GetSubnetParamFromPath(vpcList.Items[0].Status.NSXResourcePath)
 
 	// A hack as there is no vpc controller for now.
+	parameters := subnet.SubnetParameters{}
 	parameters.OrgID = "default"
 	parameters.ProjectID = "dy_project_1"
 	parameters.VPCID = "vpc_test_1"
